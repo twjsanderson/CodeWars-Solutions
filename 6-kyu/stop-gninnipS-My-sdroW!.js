cf697f5320f84fe7051d4ee5118f9b16732da73f
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2021-02-15 16:50:55 -0500

    add comments to exercise

--- 6-kyu/stop-gninnipS-My-sdroW!.js
+++ 6-kyu/stop-gninnipS-My-sdroW!.js
@@ -1,7 +1,14 @@
 function spinWords(words) {
+    // Create an arr of words
     let arr = words.split(" ");
+
+    // Loop through the arr
     for (let i = 0; i < arr.length; i++) {
+      // If the word > 4 letters long, turn the word into an array, reverse it 
+      // and turn it back into a string in place
       if (arr[i].length > 4) arr[i] = arr[i].split("").reverse().join("");
     }; 
+
+    // Return the mutated arr joined back into a string
     return arr.join(" ");
 };
